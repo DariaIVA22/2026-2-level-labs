7c1cd72f9a65e49d66026ff6e57276edf7b3b889
DariaIVA22 <myhusbandshrek@gmail.com>
2026-09-24 22:01:52 +0300

    start.py step 4

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-
+from lab_1_classify_profile.main import (tokenize, remove_stop_words, calculate_frequencies, get_top_n_words)
 
 def main() -> None:
     """
@@ -17,7 +17,20 @@ def main() -> None:
         stopwords = file.read().split("\n")
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
-    result = None
+
+    tokens = tokenize(de_text)
+    print("Токены:", tokens)
+
+    clean_tokens = remove_stop_words(tokens, stopwords)
+    print("Токены без стоп-слов:", clean_tokens)
+
+    freq_dict = calculate_frequencies(clean_tokens)
+    print("Частотный словарь:", freq_dict)
+
+    top_7 = get_top_n_words(freq_dict, 7)
+    print("Топ-7 популярных слов текста:", top_7)
+
+    result = top_7
     assert result, "Detection result is None"
 
 
